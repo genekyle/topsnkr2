40cbfd6892b994d9e5c26b386def08351d7f60b1
genekyle <genomags@gmail.com>
2022-08-18 20:33:37 -0400

    added to product page

--- topsneakers/pages/product/[slug].js
+++ topsneakers/pages/product/[slug].js
@@ -21,12 +21,38 @@ export default function ProductPage() {
         <div className="grid md:grid-cols-4 md:gap-3">
             <div className="md:col-span-2">
             <Image
+                id="featured"
                 src={product.image}
                 alt={product.name}
                 width={640}
                 height={640}
                 layout="responsive"
             ></Image>
+            <div className="flex space-x-2">
+                <div class = "thumbnail active" className="relative sm:h-[100px] sm:w-[100px] sm:border-solid sm:border-2 md:h-[150px] md:w-[150px]">
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-full hover:cursor-pointer hover:bg-[#F5F5DC]"
+                    ></Image>
+                </div>
+                <div class = "thumbnail" className="relative sm:h-[100px] sm:w-[100px] sm:border-solid sm:border-2 md:h-[150px] md:w-[150px]"> 
+                    <Image
+                        src="/images/pandalows2.png"
+                        layout="fill"
+                        className="rounded-full hover:cursor-pointer hover:bg-[#F5F5DC]"
+                        ></Image>
+                </div>
+                <div class = "thumbnail" className="relative sm:h-[100px] sm:w-[100px] sm:border-solid sm:border-2 md:h-[150px] md:w-[150px]"> 
+                    <Image
+                        src="/images/pandalows3.png"
+                        layout="fill"
+                        className="rounded-full hover:cursor-pointer hover:bg-[#F5F5DC]"
+                        ></Image>
+                </div>
+            </div>
             </div>
             <div>
                 <ul>
